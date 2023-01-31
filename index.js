6eb40d7fdaf10b42d288e55065c31f8fa3858781
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-01-30 19:48:59 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// using JS changing innerHTML by h1 tag
+document.querySelector("h1").innerHTML = "Masud Rana";
+// using JQuery changing innerHTML by h1 tag
+//step1: use $ symbol override document.querySelector
+//step2: use text override innerHTML
+// step3: after text use ("something write here what you change your tag")
+$("h1").text("Mukta Thakur");
